8ffe7bb5818a40f472b7c9a0e7155468164cf291
jetzerszeri <jetzers.2@gmail.com>
2023-10-16 17:07:36 -0400

    feat: create function to validateToken
    and require authorization

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const jwt = require('jsonwebtoken');
+const clave = 'appkey'; //clave para el token, debe ir en un archivo aparte
+
+
+
+function validateToken(req, res, next) {
+    let token = req.headers.authorization;
+
+    //me aseguro que si pasó el token
+    if (!token){
+        return res.status(401).json({message: 'Acceso no autorizado'});
+    }else{
+        token = token.split(' ')[1];
+    }
+
+    console.log(token);
+
+    jwt.verify(token, clave, (error, decoded) => {
+        if (error) {
+            console.log(error.JsonWebTokenError);
+            return res.status(403).json({message: 'Token inválido'});
+        }
+
+        //si el token valida, guardo el id del usuario en el request para usarlo luego
+        req.userId = decoded.userId;
+        console.log('userId: ' + req.userId)
+        next();
+    });
+}
+
+
+
 
 router.get('/', (req, res) => {
     res.status(200).json({
