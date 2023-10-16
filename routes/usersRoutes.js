c5cf69249eeb5a7acab53067f7f62d5efb43389c
jetzerszeri <jetzers.2@gmail.com>
2023-10-16 15:25:11 -0400

    test that the router works

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'probandoo la ruta desde usuario'
+    });
+});
 
 module.exports = router;
