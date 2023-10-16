ad9c9439ab262268e4d828dcb301b4c1b8eddda1
jetzerszeri <jetzers.2@gmail.com>
2023-10-16 16:39:12 -0400

    create route to add user

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController');
 
 router.get('/', (req, res) => {
     res.status(200).json({
         message: 'probandoo la ruta desde usuario'
     });
 });
+
+router.post('/', userController.addUser);
 
 module.exports = router;
