6ebee945865d390a2e5265880d88b4095991de21
jetzerszeri <jetzers.2@gmail.com>
2023-10-16 19:15:08 -0400

    feat: add vendors router

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const usersRoutes = require('./usersRoutes.js');
+const vendorsRoutes = require('./vendorsRoutes.js');
 
 const routerApi = function (app) {
     app.use('/api/users', usersRoutes);
+    app.use('/api/vendors', vendorsRoutes);
 }
 
 module.exports = routerApi;
-
