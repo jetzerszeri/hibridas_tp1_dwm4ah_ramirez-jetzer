c3a6b8e066f6fe913ae76bc0db3b30953577108f
jetzerszeri <jetzers.2@gmail.com>
2023-10-16 22:29:13 -0400

    feat: add contracts routes file

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ const usersRoutes = require('./usersRoutes.js');
 const vendorsRoutes = require('./vendorsRoutes.js');
 const customersRoutes = require('./customersRoutes.js');
 const projectsRoutes = require('./projectsRoutes.js');
+const contractsRoutes = require('./contractsRoutes.js');
 
 const routerApi = function (app) {
     app.use('/api/users', usersRoutes);
     app.use('/api/vendors', vendorsRoutes);
     app.use('/api/customers', customersRoutes);
     app.use('/api/projects', projectsRoutes);
+    app.use('/api/contracts', contractsRoutes);
 }
 
 module.exports = routerApi;
