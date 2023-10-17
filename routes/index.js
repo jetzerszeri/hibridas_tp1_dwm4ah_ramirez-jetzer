1d5347a18010ad6aba86e35c4b71c955303f4f92
jetzerszeri <jetzers.2@gmail.com>
2023-10-16 21:40:32 -0400

    feat: add projects routes file

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const usersRoutes = require('./usersRoutes.js');
 const vendorsRoutes = require('./vendorsRoutes.js');
 const customersRoutes = require('./customersRoutes.js');
+const projectsRoutes = require('./projectsRoutes.js');
 
 const routerApi = function (app) {
     app.use('/api/users', usersRoutes);
     app.use('/api/vendors', vendorsRoutes);
     app.use('/api/customers', customersRoutes);
+    app.use('/api/projects', projectsRoutes);
 }
 
 module.exports = routerApi;
