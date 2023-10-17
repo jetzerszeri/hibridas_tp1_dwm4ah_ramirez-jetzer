f2812dd288259672be0db5d4bc0e648d7db95743
jetzerszeri <jetzers.2@gmail.com>
2023-10-16 20:56:28 -0400

    feat: add customers routes

---
+++ routes/customersRoutes.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+const customerController = require('../controllers/customerController');
+const jwt = require('jsonwebtoken');
+const clave = 'appkey'; //clave para el token, debe ir en un archivo aparte
+
+function validateToken(req, res, next) {
+    let token = req.headers.authorization;
+
+    //me aseguro que si pasó el token
+    if (!token){
+        return res.status(401).json({message: 'Acceso no autorizado'});
+    }else{
+        token = token.split(' ')[1];
+    }
+
+    // console.log(token);
+
+    jwt.verify(token, clave, (error, decoded) => {
+        if (error) {
+            console.log(error.JsonWebTokenError);
+            return res.status(403).json({message: 'Token inválido'});
+        }
+
+        //si el token valida, guardo el id del usuario en el request para usarlo luego
+        req.userId = decoded.userId;
+        console.log('userId: ' + req.userId)
+        next();
+    });
+};
+
+//aquí van las rutas...
+
+
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const usersRoutes = require('./usersRoutes.js');
 const vendorsRoutes = require('./vendorsRoutes.js');
+const customersRoutes = require('./customersRoutes.js');
 
 const routerApi = function (app) {
     app.use('/api/users', usersRoutes);
     app.use('/api/vendors', vendorsRoutes);
+    app.use('/api/customers', customersRoutes);
 }
 
 module.exports = routerApi;
